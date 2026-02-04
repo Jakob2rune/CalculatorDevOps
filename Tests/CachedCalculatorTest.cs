bd3fbae880f93d4b9ca6c30030f6abdcaf704571
Jakob2rune <jakjes01@easv365.dk>
2026-02-04 08:28:15 +0100

    Created more tests

--- Tests/CachedCalculatorTest.cs
+++ Tests/CachedCalculatorTest.cs
@@ -18,4 +18,60 @@ public class CachedCalculatorTest
         // Assert
         Assert.That(result, Is.EqualTo(5));
     }
+    [Test]
+    public void Subtract()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 3;
+        var b = 3;
+
+        // Act
+        var result = calc.Subtract(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(0));
+    }
+    [Test]
+    public void multiply()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 3;
+        var b = 3;
+
+        // Act
+        var result = calc.Multiply(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(6));
+    }
+    [Test]
+    public void Divide()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 3;
+        var b = 3;
+
+        // Act
+        var result = calc.Divide(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+    [Test]
+    public void isPrime()
+    {
+        // Arrange
+        var calc = new CachedCalculator();
+        var a = 2;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(true));
+    }
+
 }
