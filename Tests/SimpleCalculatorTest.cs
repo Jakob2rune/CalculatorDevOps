f56b8d224abc7aa96b7bdf24ba4c0e83b1230520
Jakob2rune <jakjes01@easv365.dk>
2026-02-17 10:56:22 +0100

    Created More Tests for Simple Calc

--- Tests/SimpleCalculatorTest.cs
+++ Tests/SimpleCalculatorTest.cs
@@ -87,4 +87,142 @@ public class SimpleCalculatorTest
         // Assert
         Assert.That(result, Is.EqualTo(true));
     }
+    [Test]
+    public void Factorial_WithZero_ReturnsOne()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 0;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Factorial_WithOne_ReturnsOne()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 1;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Factorial_WithFive_ReturnsOneHundredTwenty()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = 5;
+
+        // Act
+        var result = calc.Factorial(n);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(120));
+    }
+
+    [Test]
+    public void Factorial_WithNegativeNumber_ThrowsArgumentException()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var n = -3;
+
+        // Act & Assert
+        var ex = Assert.Throws<ArgumentException>(() => calc.Factorial(n));
+        Assert.That(ex.Message, Is.EqualTo("Factorial is not defined for negative numbers"));
+    }
+
+    [Test]
+    public void IsPrime_WithOne_ReturnsFalse()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 1;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void IsPrime_WithTwo_ReturnsTrue()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 2;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void IsPrime_WithThree_ReturnsTrue()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 3;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void IsPrime_WithFour_ReturnsFalse()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 4;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void IsPrime_WithSeventeen_ReturnsTrue()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 17;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void IsPrime_WithNegativeNumber_ReturnsFalse()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = -5;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(false));
+        //test
+    }
 }
